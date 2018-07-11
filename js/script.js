5f5fc23879bf6f773787868d8255f0a4f1647ad9
Joanna Michalak <aska.rybnik@gmail.com>
2018-07-11 21:03:47 +0100

    Finished

--- js/script.js
+++ js/script.js
@@ -1,33 +1,43 @@
 'use strict';
 
-(function() {
-
-	var galleryTemplate = document.getElementById('gallery-template').innerHTML;
-	var galleryList = '';
-	for(var i = 0; i < galleryData.length; i++){
-			galleryList += Mustache.render(galleryTemplate, galleryData[i]);
-		}
-
-var carouselMain = document.getElementById('carousel-main');
-carouselMain.innerHTML = galleryList;
-
-var carousel = document.querySelector('.carousel');
-var flkty = new Flickity( carousel, {
-  hash: true, wrapAround: true
+var elem = document.querySelector('.main-carousel');
+var flkty = new Flickity( elem, {
+  // options
+  cellAlign: 'left',
+  contain: true
 });
 
-var progressBar = document.querySelector('.progress-bar');
+//Reset
+var buttonGroup = document.querySelector(".button-group"),
+    hashsGroup = document.querySelector(".hashs-group"),
+    buttons = buttonGroup.querySelectorAll(".button-restart"),
+    hashs = hashsGroup.getElementsByTagName("a");
+
+buttons = fizzyUIUtils.makeArray(buttons);
+hashs = fizzyUIUtils.makeArray(hashs);
+
+buttonGroup.addEventListener("click", function(event) {
+
+  // Button clicks
+  if (!matchesSelector(event.target, ".button-restart")) {
+    return;
+  }
+  var index = buttons.indexOf(event.target);
+  flkty.select(index);
+});
+
+hashsGroup.addEventListener("click", function(event) {
+  if (!matchesSelector(event.target, "a")) {
+    return;
+  }
+  var index = hashs.indexOf(event.target);
+  flkty.select(index);
+});
+
+//Scroll progress bar
+var progressBar = document.querySelector('.progress-bar')
 
 flkty.on( 'scroll', function( progress ) {
   progress = Math.max( 0, Math.min( 1, progress ) );
   progressBar.style.width = progress * 100 + '%';
-});
-
-var buttonGroup = document.querySelector('.button-group');
-
-buttonGroup.addEventListener( 'click', function( event ) {
-  if ( !matchesSelector( event.target, '.reset-btn' ) ) {
-    return;
-  }
-  flkty.select(0);
 });
